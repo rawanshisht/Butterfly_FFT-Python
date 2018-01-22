dea3f4611cb36d431596657ff71f89fdf874a486
Alzahraa Elsallakh <35645089+alzahraaelsallakh@users.noreply.github.com>
2018-01-22 21:55:26 +0200

    Update ButterFly.py

--- ButterFly.py
+++ ButterFly.py
@@ -1,24 +1,25 @@
 
 from cmath import*
 
+
 Xn = [1,1,1,1,0,1,1,1]
+#Xn is the discrete time sequence
 
 real= [None]* len(Xn)
 img= [None]* len(Xn)
 Mag= [None]* len(Xn)
 PH= [None]* len(Xn)
 
-#recursion is called log(n)times, with 2 recursion in each then big O is (2^log(n))
-#3 for loops, each is (((n/2)).. then O(n/2)
-#the whole big O of Butterfly func. is (2^log(n))+((n/2))
-
-
 def Butterfly(Xn):
     N = len(Xn)
+    
     if N == 1: 
         return Xn
+    
     if N % 2 > 0:
         print("Size Of X(n) Must Be Power Of 2")
+    
+    #decimation in time process of Xn by splitting it into even and odd indexes
     even = Butterfly(Xn[::2])
     odd =  Butterfly(Xn[1::2])
     r = range (N//2)
@@ -27,15 +28,15 @@ def Butterfly(Xn):
                [even[k] - (Wn[k]* odd[k]) for k in r])
     return Xk
 
-
-
+#The final result of Xk
 l=0
 for i in Butterfly(Xn):
  print( "X(",l,") is:\n" ,round(i.real,2)+round(i.imag,2)*1j)
  real[l]= round(i.real,2)
  img[l]= round(i.imag,2)
  l=l+1
- 
+
+#computing magnitude and phase
 for i in range (len(real)):
     Mag[i]= round(sqrt(real[i]**2+ img[i]**2).real,2)    
     PH[i]= round(atan(img[i]/real[i]).real,2)
